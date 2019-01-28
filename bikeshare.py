aa6d63b8165d51a439c67a5765c8f0de9b3c0501
Carlos Andre da Costa Sol <andre.costasol@gmail.com>
2019-01-27 19:26:31 -0500

    Add Final Message

--- bikeshare.py
+++ bikeshare.py
@@ -390,8 +390,9 @@ def main():
         
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
+            print("Thank you for exploring Bikeshare Data!")
             break
-
+            
 
 if __name__ == "__main__":
 	main()
